f25ebe7bcc51e726a30cdcb7c8e05122cddd7f74
JLLormeau <40337213+JLLormeau@users.noreply.github.com>
2019-12-02 19:14:32 +0100

    Update azure-cli-delete-labs-ressource-group.sh

--- azure-cli-delete-labs-ressource-group.sh
+++ azure-cli-delete-labs-ressource-group.sh
@@ -13,8 +13,11 @@ then
                         VM_RESSOURCE_GROUP=$DOMAIN_NAME'_'$USER
                         ACR_RESSOURCE_GROUP='acr'$DOMAIN_NAME'0'$i
                         AKS_RESSOURCE_GROUP='MC_'$ACR_RESSOURCE_GROUP'_akscluster_westeurope'
+                        echo "delete ressource group "$VM_RESSOURCE_GROUP
                         az group delete --name $VM_RESSOURCE_GROUP --y
+                        echo "delete ressource group "$ACR_RESSOURCE_GROUP
                         az group delete --name $ACR_RESSOURCE_GROUP --y
+                        echo "delete ressource group "$AKS_RESSOURCE_GROUP
                         az group delete --name $AKS_RESSOURCE_GROUP --y
                 done
 else
@@ -22,6 +25,7 @@ else
                 do
                         USER='USER0'$i
                         VM_RESSOURCE_GROUP=$DOMAIN_NAME'_'$USER
+                        echo "delete ressource group "$VM_RESSOURCE_GROUP
                         az group delete --name $VM_RESSOURCE_GROUP --y
                 done
 fi
