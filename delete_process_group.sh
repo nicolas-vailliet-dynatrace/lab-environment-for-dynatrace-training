6f0d5acc956f7f39a5df4e5516f0bcabcd6f779c
JLLormeau <40337213+JLLormeau@users.noreply.github.com>
2019-12-02 18:43:13 +0100

    Update delete_process_group.sh

--- delete_process_group.sh
+++ delete_process_group.sh
@@ -2,22 +2,23 @@
 read DOMAIN
 read NBVM
 read AKS
-echo "continue (Y/N)"
-read Response
-if [ $Response = "N" ] || [ $Response = "n" ]
+if [ $AKS = "Y" ] || [ $AKS = "y" ]
 then
-	exit
-fi
-
-for ((i=0; i<$NBVM; ++i));
-	do
-		USER='USER0'$i
-		VM_RESSOURCE_GROUP=$DOMAIN_NAME'_'$USER
-		ACR_RESSOURCE_GROUP='acr'$DOMAIN_NAME'0'$i
-		AKS_RESSOURCE_GROUP='MC_'$ACR_RESSOURCE_GROUP'_akscluster_westeurope'
-		echo 'delere  : ' $DOMAIN'.'$LOCATION'.cloudapp.azure.com'
-		az group delete --name $VM_RESSOURCE_GROUP
-		az group delete --name $ACR_RESSOURCE_GROUP
-		az group delete --name $AKS_RESSOURCE_GROUP
-	done
-fi
+	for ((i=0; i<$NBVM; ++i));
+		do
+			USER='USER0'$i
+			VM_RESSOURCE_GROUP=$DOMAIN_NAME'_'$USER
+			ACR_RESSOURCE_GROUP='acr'$DOMAIN_NAME'0'$i
+			AKS_RESSOURCE_GROUP='MC_'$ACR_RESSOURCE_GROUP'_akscluster_westeurope'
+			az group delete --name $VM_RESSOURCE_GROUP
+			az group delete --name $ACR_RESSOURCE_GROUP
+			az group delete --name $AKS_RESSOURCE_GROUP
+		done
+else
+	for ((i=0; i<$NBVM; ++i));
+		do
+			USER='USER0'$i
+			VM_RESSOURCE_GROUP=$DOMAIN_NAME'_'$USER
+			az group delete --name $VM_RESSOURCE_GROUP
+		done
+fi	
