fddcb90331577ba53cfaa74d8453581fa522c642
JLLormeau <40337213+JLLormeau@users.noreply.github.com>
2019-12-06 14:49:18 +0100

    Update azure-cli-delete-labs-ressource-group.sh

--- azure-cli-delete-labs-ressource-group.sh
+++ azure-cli-delete-labs-ressource-group.sh
@@ -10,22 +10,22 @@ then
         for ((i=0; i<$NBVM; ++i));
                 do
                         USER='USER0'$i
-                        VM_RESSOURCE_GROUP=$DOMAIN_NAME'0'$i
-                        ACR_RESSOURCE_GROUP='acr'$DOMAIN_NAME'0'$i
-                        AKS_RESSOURCE_GROUP='MC_'$ACR_RESSOURCE_GROUP'_akscluster_westeurope'
-                        echo "delete ressource group "$VM_RESSOURCE_GROUP
-                        az group delete --name $VM_RESSOURCE_GROUP --y
-                        echo "delete ressource group "$ACR_RESSOURCE_GROUP
-                        az group delete --name $ACR_RESSOURCE_GROUP --y
-                        echo "delete ressource group "$AKS_RESSOURCE_GROUP
-                        az group delete --name $AKS_RESSOURCE_GROUP --y
+                        VM_RESOURCE_GROUP=$DOMAIN_NAME'0'$i
+                        ACR_RESOURCE_GROUP='acr'$DOMAIN_NAME'0'$i
+                        AKS_RESOURCE_GROUP='MC_'$ACR_RESOURCE_GROUP'_akscluster_westeurope'
+                        echo "delete resource group "$VM_RESOURCE_GROUP
+                        az group delete --name $VM_RESOURCE_GROUP --y
+                        echo "delete resource group "$ACR_RESOURCE_GROUP
+                        az group delete --name $ACR_RESOURCE_GROUP --y
+                        echo "delete resource group "$AKS_RESOURCE_GROUP
+                        az group delete --name $AKS_RESOURCE_GROUP --y
                 done
 else
         for ((i=0; i<$NBVM; ++i));
                 do
                         USER='USER0'$i
-                        VM_RESSOURCE_GROUP=$DOMAIN_NAME'_'$USER
-                        echo "delete ressource group "$VM_RESSOURCE_GROUP
-                        az group delete --name $VM_RESSOURCE_GROUP --y
+                        VM_RESOURCE_GROUP=$DOMAIN_NAME'_'$USER
+                        echo "delete resource group "$VM_RESOURCE_GROUP
+                        az group delete --name $VM_RESOURCE_GROUP --y
                 done
 fi
